f59b3ee8d105262deac37b551f442bf57556d1f7
Djuan Ellis <djuan.ellis@gmail.com>
2018-03-18 18:47:43 -0400

    added popup email sign up

--- image.js
+++ image.js
@@ -1,15 +1,32 @@
-$(document).ready(function() {
-$('#bs-nas-demo').click(function(e) {
-$(this).toggleClass('active');
-$('.data-target').toggleClass('active');
-e.preventDefault();
-});
-});
+// $(document).ready(function() {
+// $('#bs-nas-demo').click(function(e) {
+// $(this).toggleClass('active');
+// $('.data-target').toggleClass('active');
+// e.preventDefault();
+// });
+// });
 
 // $(".navbar-fixed-top").click(function{
 //   $(".navbar-fixed-top").slideUp();
 // });
-$(".navbar-fixed-top").on("click", function(){
-	 $(".navbar-fixed-top").slideUp();
-	alert("navbar-right was clicked!");
+// $(".navbar-fixed-top").on("click", function(){
+// 	 $(".navbar-fixed-top").slideUp();
+// 	alert("navbar-right was clicked!");
+// });
+$(function() {
+
+  
+  $('.contact').click(function() {
+    $('.contactForm').fadeToggle();
+  })
+  $(document).mouseup(function (e) {
+    var container = $(".contactForm");
+
+    if (!container.is(e.target)
+        && container.has(e.target).length === 0)
+    {
+        container.fadeOut();
+    }
+  });
+
 });
